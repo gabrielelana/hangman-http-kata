70b72554e264bb3d06f6116080252c20b19bce25
gabriele.lana <gabriele.lana@cleancode.it>
2014-06-30 19:15:30 +0200

    [take-002] Create User document after POST /me

--- take-002-nodejs-server-clone/server.js
+++ take-002-nodejs-server-clone/server.js
@@ -1,5 +1,7 @@
 var express = require('express'),
     mongoose = require('mongoose'),
+    util = require('util'),
+    _ = require('lodash'),
     app = express()
 
 app.set('port', process.env.PORT || 3000)
@@ -12,14 +14,35 @@ if ('test' === app.get('env')) {
 
 mongoose.connect(app.get('mongodb-url'))
 
+var User = mongoose.model('User', (function() {
+  return mongoose.Schema({
+    'email_address': {type: String, required: 'Missing required field [{PATH}]'},
+    'password': {type: String, required: 'Missing required field [{PATH}]'}
+  })
+})())
+
 app.use(require('morgan')('dev'))
-app.use(require('body-parser').json())
+app.use(require('body-parser').urlencoded({extended: true}))
 
 app.get('/', function(req, res) {
   res.json({
     'index': '/',
     'me': '/me',
     'prisoners': '/prisoners'
+  })
+})
+
+app.post('/me', function(req, res) {
+  new User(req.body).save(function(err, user) {
+    if (err) {
+      return res.send(400, {
+        'status': 'Bad Request',
+        'status_code': 400,
+        'description': _(err.errors).chain().values().pluck('message').join(', ').value()
+      })
+    }
+    res.location(util.format('/users/%s', user.id))
+    res.send(201)
   })
 })
 
